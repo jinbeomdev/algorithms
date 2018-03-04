c2d63b1bb7801b1f3b606515b927f682d2438b7a
Jinbeomdev <niutn@naver.com>
2018-03-04 15:08:10 +0900

    solved [BOJ]5427

--- [BOJ]5427/Source.cpp
+++ [BOJ]5427/Source.cpp
@@ -63,6 +63,7 @@ IMPOSSIBLE
 #include <iostream>
 #include <queue>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
@@ -71,6 +72,9 @@ const int iter_y[] = {0, 1, 0, -1};
 const int iter_x[] = {1, 0, -1, 0};
 const int iter_size = 4;
 
+char adj[1000][1000];
+int dist[1000][1000];
+
 int main() {
 	int T, test_case;
 
@@ -78,8 +82,7 @@ int main() {
 
 	for (test_case = 0; test_case < T; test_case++) {
 		int w, h;
-		char adj[1000][1000];
-		int dist[1000][1000];
+
 		//queue for bfs to find shortest path that is connected to exit
 		queue<pair<int, int>> q;
 		//queue for fire
@@ -100,7 +103,7 @@ int main() {
 					adj[i][j] = '.';
 				}
 
-				if(adj[i][j] == '*') {
+				if (adj[i][j] == '*') {
 					fire_q.push({ i, j });
 				}
 			}
@@ -110,11 +113,10 @@ int main() {
 		q.push(starting_location);
 		dist[starting_location.first][starting_location.second] = 0;
 
-		if (!q.empty()) {
-			//current position
-			int y = q.front().first;
-			int x = q.front().second;
-			q.pop();
+		while (true) {
+			if (q.empty()) {
+				break;
+			}
 
 			//let fire spread to all direction
 			int fire_q_size = fire_q.size();
@@ -122,41 +124,51 @@ int main() {
 			for (int i = 0; i < fire_q_size; i++) {
 				int fire_y = fire_q.front().first;
 				int fire_x = fire_q.front().second;
-				q.pop();
+				fire_q.pop();
 
 				for (int j = 0; j < iter_size; j++) {
 					int spreaded_fire_y = fire_y + iter_y[j];
 					int spreaded_fire_x = fire_x + iter_x[j];
-					
+
 					if (spreaded_fire_y >= 0 && spreaded_fire_y < h &&
 						spreaded_fire_x >= 0 && spreaded_fire_x < w &&
 						adj[spreaded_fire_y][spreaded_fire_x] == '.') {
 
 						adj[spreaded_fire_y][spreaded_fire_x] = '*';
-						q.push({ spreaded_fire_y, spreaded_fire_x });
+						fire_q.push({ spreaded_fire_y, spreaded_fire_x });
 					}
 				}
 			}
 
-			for (int i = 0; i < 4; i++) {
-				int next_y = y + iter_y[i];
-				int next_x = x + iter_x[i];
+			int q_size = q.size();
+			
+			for (int k = 0; k < q_size; k++) {
+				//current position
+				int y = q.front().first;
+				int x = q.front().second;
+				q.pop();
 
-				if (next_y >= 0 && next_y < h &&
-					next_x >= 0 && next_x < w &&
-					dist[next_y][next_x] == -1 &&
-					adj[next_y][next_x] == '.') {
-					q.push({ next_y, next_x });
-					dist[next_y][next_x] = dist[y][x] + 1;
+				for (int i = 0; i < 4; i++) {
+					int next_y = y + iter_y[i];
+					int next_x = x + iter_x[i];
+
+					if (next_y >= 0 && next_y < h &&
+						next_x >= 0 && next_x < w &&
+						dist[next_y][next_x] == -1 &&
+						adj[next_y][next_x] == '.') {
+						q.push({ next_y, next_x });
+						dist[next_y][next_x] = dist[y][x] + 1;
+					}
 				}
 			}
 		}
 
 		int ret = 10e6;
+
 		for (int i = 0; i < w; i++) {
 			if (dist[0][i] != -1) {
 				ret = min(ret, dist[0][i]);
-		  }
+			}
 		}
 
 		for (int i = 0; i < w; i++) {
@@ -178,13 +190,13 @@ int main() {
 		}
 
 		if (ret != 10e6) {
-			cout << ret << endl;
+			cout << ret + 1 << endl;
 		}
 		else {
 			cout << "IMPOSSIBLE" << endl;
 		}
-
 	}
+	//system("pause");
 
 	return 0;
 }
