a931954a256d088e58384dfb1a66a83c60bdc606
Jinbeomdev <niutn@naver.com>
2018-04-12 10:14:10 +0900

    init

---
+++ [SWEA]1953/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int test_case_size;
+
+	cin >> test_case_size;
+
+	for (int test_case = 1; test_case <= test_case_size; test_case++) {
+		int N, M, R, C, L;
+
+	}
+}
