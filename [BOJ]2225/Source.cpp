f5e6d174c0dd8999d7d19dc2e1133c3c8fb9dd89
Jinbeomdev <niutn@naver.com>
2018-06-30 16:13:52 +0900

    ?

--- [BOJ]2225/Source.cpp
+++ [BOJ]2225/Source.cpp
@@ -22,6 +22,28 @@
 
 using namespace std;
 
-int main() {
+const int kMod = 10e9;
 
+int dp[201][201] = {};
+
+int main() {
+	int n, k;
+
+	cin >> n >> k;
+
+	for (int i = 0; i <= n; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= k; i++) {
+		for (int j = 0; j <= n; j++) {
+			for (int k = 0; k <= j; k++) {
+				dp[i][j] += dp[i - 1][j - k] % kMod;
+			}
+		}
+	}
+
+	cout << dp[k][n]%kMod;
+
+	system("pause");
 }
