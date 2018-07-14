402f0b03bf55c31de72febfb49351bbaa6056485
Jinbeomdev <niutn@naver.com>
2018-07-14 14:22:57 +0900

    solve

--- [BOJ]2661/Source.cpp
+++ [BOJ]2661/Source.cpp
@@ -1,46 +1,63 @@
 #include <cstdio>
 #include <cstdlib>
 
+bool found = false;
 int length;
 int permutation[80];
 
-bool find(int idx) {
-	if (idx == length) {
-		//좋은 수열인지 판단
-		bool is_good = true;
-		for (int i = 0; i < length; i++) {
-			if (i == 0 && abs(permutation[0] - permutation[1]) != 1) {
-				is_good = false;
-			} else if (i == length - 1 && abs(permutation[length - 1] - permutation[length - 2]) == 1) {
-				is_good = false;
-			} else if (abs(permutation[i] - permutation[i - 1]) != 1 || abs(permutation[i] - permutation[i + 1]) != 1) {
-				is_good = false;
+bool check(int idx) {
+	if (idx == 0) return true;
+
+	if (permutation[idx] == permutation[idx - 1]) {
+		return false;
+	}
+
+	int size = (idx + 1) / 2;
+
+	if (size < 2) return true;
+
+	for (int i = 2; i <= size; i++) {
+		bool ok = true;
+
+		for (int j = 0; j < i; j++) {
+			if (permutation[idx - j] != permutation[idx - j - i]) {
+				ok = false;
+				break;
 			}
 		}
 
-		if (is_good) {
-			for (int i = 0; i < length; i++) {
-				printf("%d", permutation[i]);
-			}
-			return true;
-		}
-		else {
-			return false;
-		}
+		if (true) return false;
 	}
-	
+
+	return true;
+}
+
+void find(int idx) {
+	if (idx == length) {
+		found = true;
+		return;
+	}
+
 	for (int i = 1; i <= 3; i++) {
 		permutation[idx] = i;
-		if(find(idx + 1)) return true;
-	}
+		
+		if (check(idx)) {
+			find(idx + 1);
+			if (found) break;
+		}
 
-	return false;
+
+	}
 }
 
 int main() {
 	scanf("%d", &length);
 
 	find(0);
+
+	for (int i = 0; i < length; i++) {
+		printf("%d", permutation[i]);
+	}
 
 	return 0;
 }
