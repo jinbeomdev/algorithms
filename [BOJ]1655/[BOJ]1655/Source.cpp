5eeda52bf8253257edac793dd656471fabe2021e
Jinbeomdev <niutn@naver.com>
2018-07-17 15:05:14 +0900

    solve

--- [BOJ]1655/[BOJ]1655/Source.cpp
+++ [BOJ]1655/[BOJ]1655/Source.cpp
@@ -1,44 +1,40 @@
 #include <cstdio>
 #include <queue>
+#include <functional>
 
 using namespace std;
 
 int N;
-priority_queue<int> pq;
-priority_queue<int> temp;
+priority_queue<int, vector<int>, greater<int>> min_heap;
+priority_queue<int, vector<int>, less<int>> max_heap;
 
 int main() {
 	scanf("%d", &N);
 
 	for (int i = 0; i < N; i++) {
 		int input;
-
+		
 		scanf("%d", &input);
 
-		pq.push(-input);
-
-		int pq_size = pq.size();
-		
-		if (pq_size % 2 == 0) {
-			pq_size = pq_size / 2 - 1;
+		if (max_heap.empty()) {
+			max_heap.push(input);
+		} else if (max_heap.size() == min_heap.size()) {
+			max_heap.push(input);
 		} else {
-			pq_size = pq_size / 2;
+			min_heap.push(input);
 		}
 
-		for (int j = 0; j < pq_size; j++) {
-			temp.push(-pq.top());
-			pq.pop();
+		if (!max_heap.empty() &&
+				!min_heap.empty() &&
+				!(max_heap.top() <= min_heap.top())) {
+			int max = max_heap.top();
+			int min = min_heap.top();
+			max_heap.pop();
+			min_heap.pop();
+			max_heap.push(min);
+			min_heap.push(max);
 		}
 
-		printf("%d\n", -pq.top());
-
-		while (!temp.empty()) {
-			pq.push(-temp.top());
-			temp.pop();
-		}
+		printf("%d\n", max_heap.top());
 	}
 }
-
-/*
-	순회하는 heap 내가 만들어야 할듯?
-*/
