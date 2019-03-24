cfcfa9b6f859641df79be55b2b741c51a85cff6a
Jinbeomdev <niutn@naver.com>
2019-03-24 10:01:45 +0900

    solve

--- [BOJ]12100/[BOJ]12100/Source.cpp
+++ [BOJ]12100/[BOJ]12100/Source.cpp
@@ -1,169 +1,84 @@
-#include <cstdio>
+#include <stdio.h>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int N;
-int biggest_block;
+int map[20][20];
 
-void Save(int origin[21][21], int board[21][21]) {
-	for (int i = 0; i < 21; i++) {
-		for (int j = 0; j < 21; j++) {
-			origin[i][j] = board[i][j];
-		}
-	}
-}
-
-void Move(int board[21][21], int dir) {
-	if (dir == 3) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 1; j < N; j++) {
-				bool is_moved = false;
-				int cur_y = i, cur_x = j;
-
-				while (board[cur_y][cur_x - 1] == 0) {
-					is_moved = true;
-					cur_x--;
-				}
-
-				if (is_moved) {
-					board[cur_y][cur_x] = board[i][j];
-					board[i][j] = 0;
-				}
-			}
-		}
-	} else if (dir == 1) {
-		for (int i = 0; i < N; i++) {
-			for (int j = N - 2; j >= 0; j--) {
-				bool is_moved = false;
-				int cur_y = i, cur_x = j;
-
-				while (board[cur_y][cur_x + 1] == 0) {
-					is_moved = true;
-					cur_x++;
-				}
-
-				if (is_moved) {
-					board[cur_y][cur_x] = board[i][j];
-					board[i][j] = 0;
-				}
-			}
-		}
-	} else if (dir == 0) {
-		for (int j = 0; j < N; j++) {
-			for (int i = 1; i < N; i++) {
-				bool is_moved = false;
-				int cur_y = i, cur_x = j;
-
-				while (board[cur_y - 1][cur_x] == 0) {
-					is_moved = true;
-					cur_y--;
-				}
-
-				if (is_moved) {
-					board[cur_y][cur_x] = board[i][j];
-					board[i][j] = 0;
-				}
-			}
-		}
-	} else if (dir == 2) {
-		for (int j = 0; j < N; j++) {
-			for (int i = N - 2; i >= 0; i--) {
-				bool is_moved = false;
-				int cur_y = i, cur_x = j;
-
-				while (board[cur_y + 1][cur_x] == 0) {
-					is_moved = true;
-					cur_y++;
-				}
-
-				if (is_moved) {
-					board[cur_y][cur_x] = board[i][j];
-					board[i][j] = 0;
-				}
-			}
-		}
-	}
-}
-
-void Merge(int board[21][21], int dir) {
-	if (dir == 3) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N - 1; j++) {
-				if (board[i][j] == board[i][j + 1]) {
-					board[i][j] = board[i][j] + board[i][j + 1];
-					board[i][j + 1] = 0;
-				}
-			}
-		}
-	} else if (dir == 1) {
-		for (int i = 0; i < N; i++) {
-			for (int j = N - 1; j > 0; j--) {
-				if (board[i][j] == board[i][j - 1]) {
-					board[i][j] = board[i][j] + board[i][j - 1];
-					board[i][j - 1] = 0;
-				}
-			}
-		}
-	} else if (dir == 0) {
-		for (int j = 0; j < N; j++) {
-			for (int i = 0; i < N - 1; i++) {
-				if (board[i][j] == board[i + 1][j]) {
-					board[i][j] = board[i][j] + board[i + 1][j];
-					board[i + 1][j] = 0;
-				}
-			}
-		}
-	} else if (dir == 2) {
-		for (int j = 0; j < N; j++) {
-			for (int i = N - 2; i > 0; i--) {
-				if (board[i][j] == board[i - 1][j]) {
-					board[i][j] = board[i][j] + board[i - 1][j];
-					board[i - 1][j] = 0;
-				}
-			}
-		}
-	}
-}
-
-void dfs(int cnt_move, int board[21][21]) {
-
-	if (cnt_move > 5) {
+int dfs(int idx) {
+	if (idx == 5) {
+		int ret = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				biggest_block = max(biggest_block, board[i][j]);
+				ret = max(ret, map[i][j]);
 			}
 		}
-		return;
+		return ret;
 	}
 
-	int origin[21][21];
-	Save(origin, board);
+	int ret = 0;
+	int temp[20][20];
+	for (int j = 0; j < N; j++) {
+		for (int k = 0; k < N; k++) {
+			temp[j][k] = map[j][k];
+		}
+	}
 
 	for (int i = 0; i < 4; i++) {
-			Move(board, i);
-			Merge(board, i);
-			Move(board, i);
-			dfs(cnt_move + 1, board);
-			Save(board, origin);
+		for (int j = 0; j < N; j++) {
+			vector<int> v;
+			for (int k = 0; k < N; k++) {
+				if (i < 2 && map[j][k] != 0) {
+					v.push_back(map[j][k]);
+				}
+
+				if (i >= 2 && map[k][j] != 0) {
+					v.push_back(map[k][j]);
+				}
+			}
+
+			if (i == 1 || i == 3) reverse(v.begin(), v.end());
+
+			vector<int> m;
+			for (int k = 0; k < v.size(); k++) {
+				if (k + 1 < v.size() && v[k] == v[k + 1]) {
+					m.push_back(v[k] * 2);
+					k++;
+				} else {
+					m.push_back(v[k]);
+				}
+			}
+
+			for (int k = m.size(); k < N; k++) m.push_back(0);
+
+			if (i == 1 || i == 3) reverse(m.begin(), m.end());
+
+			for (int k = 0; k < N; k++) {
+				if (i < 2) map[j][k] = m[k];
+				if (i >= 2) map[k][j] = m[k];
+			}
+		}
+
+		ret = max(ret, dfs(idx + 1));
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				map[j][k] = temp[j][k];
+			}
+		}
 	}
+
+	return ret;
 }
 
 int main() {
-	biggest_block = 0;
-	int board[21][21];
 	scanf("%d", &N);
-
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			scanf("%d", &board[i][j]);
+			scanf("%d", &map[i][j]);
 		}
 	}
-	
-	if (N == 1) {
-		printf("%d", board[0][0]);
-	} else {
-		dfs(0, board);
-		printf("%d", biggest_block);
-	}
+
+	printf("%d", dfs(0));
 }
