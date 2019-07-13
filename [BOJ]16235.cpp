c15fb81a6f3b71fcad6eca15043128e38f18c9b3
jinbeom <niutn@naver.com>
2019-07-13 18:29:16 +0900

    solve

--- [BOJ]16235.cpp
+++ [BOJ]16235.cpp
@@ -29,7 +29,7 @@ int main() {
         int x, y, z;
         scanf("%d%d%d", &x, &y, &z);
         x--, y--;
-        treeMap[y][x].push_back(z);
+        treeMap[x][y].push_back(z);
     }
 
     while(K--) {
