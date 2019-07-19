1f034a47e8dfdc94eea62678fdf49a5cc521e6c3
jinbeom <niutn@naver.com>
2019-07-20 00:51:22 +0900

    solve

--- [BOJ]16137.cpp
+++ [BOJ]16137.cpp
@@ -1,21 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <queue>
 
 using namespace std;
 
 struct Data {
     int y, x, t;
-    bool o;
-    int oY, oX;
 
     Data() {}
-    Data(int _y, int _x, int _t, bool _o, int _oY = -1, int _oX = -1) {
-        y = _y, x = _x, t = _t;
-        o = _o;
-        oY = _oY;
-        oX = _oX;
-    }
+    Data(int y, int x, int t) : y(y), x(x), t(t) {}
 };
 
 const int dy[] = {-1, 0, 1, 0};
@@ -29,20 +23,9 @@ bool canBuildO (int y, int x) {
     bool h = false, v = false;
     if(y - 1 > -1 && map[y - 1][x] == 0) v = true;
     if(y + 1 < N && map[y + 1][x] == 0) v = true;
-    if(x - 1 > - 1 && map[y][x-1] == 0) h = true;
+    if(x - 1 > -1 && map[y][x - 1] == 0) h = true;
     if(x + 1 < N && map[y][x + 1] == 0) h = true;
-    return v && h;
-}
-
-void outputDebug(Data here) {
-    printf("here => y:%d, x:%d, t:%d, o:%s, oY:%d, oX:%d\n",
-    here.y,
-    here.x,
-    here.t,
-    here.o ? "true" : "false",
-    here.oY,
-    here.oX
-    );
+    return !(v && h);
 }
 
 int main() {
@@ -54,66 +37,64 @@ int main() {
         }
     }
 
-    Data answer;
-    queue<Data> q;
-    q.push(Data(0, 0, 0, false));
-    visited[0][0] = 1;
+    int answer = (int)1e8;
 
-    while(!q.empty()) { 
-        Data here = q.front();
-        q.pop();
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            if(!(map[i][j] == 0)) continue;
+            if(!canBuildO(i, j)) continue;
 
-        if(here.y == N - 1 &&
-           here.x == N - 1) {
-               answer = here;
-               break;
-        }
+            map[i][j] = M;
 
-        for(int i = 0; i < 4; i++) {
-            int nextY = here.y + dy[i];
-            int nextX = here.x + dx[i];
+            bool discovered[10][10];
+            memset(discovered, false, sizeof(discovered));
+            discovered[0][0] = true;
+            queue<Data> q;
+            q.push(Data(0, 0, 0));
 
-            if(nextY < 0 || nextY >= N) continue;
-            if(nextX < 0 || nextX >= N) continue;
+            while(!q.empty()) {
+                Data here = q.front();
+                q.pop();
 
-            if(visited[nextY][nextX]) continue;
-
-            if(map[here.y][here.x] > 1) {
-                if(map[nextY][nextX] == 0) continue;
-                if(map[nextY][nextX] > 1) continue;
-                if(here.t % map[here.y][here.x]) {
-                    Data temp = here;
-                    temp.t++;
-                    q.push(temp);
-                } else {
-                    visited[nextY][nextX] = 1;
-                    q.push(Data(nextY, nextX, here.t + 1, here.o, here.oY, here.oX));
+                if(here.y == N - 1 &&
+                   here.x == N - 1) {
+                    if(answer > here.t) {
+                        answer = here.t;
+                        break;
+                    }
                 }
-            } else if(here.o && here.y == here.oY && here.x == here.oX) {
-                if(map[nextY][nextX] == 0) continue;
-                if(map[nextY][nextX] > 1) continue;
-                if(here.t % M) {
-                    Data temp = here;
-                    temp.t++;
-                    q.push(temp);
-                } else {
-                    visited[nextY][nextX] = 1;
-                    q.push(Data(nextY, nextX, here.t + 1, here.o, here.oY, here.oX));
-                }
-            } else {
-                if(map[nextY][nextX] == 0) {
-                    if(here.o) continue;
-                    if(!canBuildO(nextY, nextX)) continue;
-                    visited[nextY][nextX] = 1;
-                    q.push(Data(nextY, nextX, here.y + 1, true, nextY, nextX));
-                } else {
 
-                    visited[nextY][nextX] = 1;
-                    q.push(Data(nextY, nextX, here.t + 1, here.o, here.oY, here.oX));
+                for(int p = 0; p < 4; p++) {
+                    int nextY = here.y + dy[p];
+                    int nextX = here.x + dx[p];
+                    
+                    if(nextY < 0 || nextY >= N) continue;
+                    if(nextX < 0 || nextX >= N) continue;
+                    if(discovered[nextY][nextX]) continue;
+                    if(map[here.y][here.x] > 1 && map[nextY][nextX] > 1) continue;
+                    if(map[nextY][nextX] == 0) continue;
+
+                    if(map[here.y][here.x] > 1) {
+                        if(here.t % map[here.y][here.x] == 0) {
+                            discovered[nextY][nextX] = true;
+                            q.push(Data(nextY, nextX, here.t + 1));
+                        } else {
+                            q.push(Data(here.y, here.x, here.t + 1));
+                        }
+                    } else {
+                        discovered[nextY][nextX] = true;
+                        q.push(Data(nextY, nextX, here.t + 1));
+                    }
                 }
             }
+        
+            map[i][j] = 0;
         }
     }
 
-    printf("%d", answer.t);
+    printf(
+        "%d", answer
+    );
+
+    return 0;
 }
